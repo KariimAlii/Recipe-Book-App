20ca99b5ef41fa69f58f88b8ceb4e1544f4ce305
Karim Ali <kariimalii96@gmail.com>
2023-01-28 12:51:47 +0200

    Using Structural & Attribute Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer: boolean = false;
-  serverCreationStatus: string = 'No Server was created!';
-  serverName:string = 'TestServer';
+  serverName: string = 'TestServer';
+  serverCreated: boolean = false;
+  serverCreationStatus: string = 'No Server Created';
+  servers: string[] = ['Test Server 1', 'Test Server 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -16,10 +18,11 @@ export class ServersComponent {
   }
   ngOnInit(): void {}
   onServerCreation() {
-    this.serverCreationStatus = 'New Server was Created! Server Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'A New Server Created with Name: ' + this.serverName;
   }
-  onUpdateServerName(event:Event) {
+  onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
-
